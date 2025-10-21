b53ef01f2fca8eb4b02c8821a167774028531d4a
himanshmunjal <145274269+himanshmunjal@users.noreply.github.com>
2025-10-22 02:21:30 +0530

    Delete Study_Material/DSA directory

--- Study_Material/DSA/ArrayList1.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class ArrayList1 {
-    public static void main(String[] args){
-        ArrayList<String> s = new ArrayList<>(1000);
-        
-        System.out.println(s.get(0));
-    }
-}
